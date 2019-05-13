4b786641934e6ccb4d0f881c47a04da364e4dbb0
pulkit.gupta <pulkit.gupta@thoughtworks.com>
2019-05-13 18:07:56 +0530

    Add FriendTest.java

---
+++ src/test/java/com/tw/expensify/FriendTest.java
@@ -0,0 +1,47 @@
+package com.tw.expensify;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FriendTest {
+    @Test
+    @DisplayName("Should settle amount for a single friend")
+    void expectsAmountToBeSettled() {
+        Friend friend = new Friend("Pulkit", 100, 0);
+        Friend settledFriend = new Friend("Pulkit", 100, 100);
+
+        friend.settleAmount(100);
+
+        Assertions.assertEquals(settledFriend, friend);
+    }
+
+    @Test
+    @DisplayName("Should settle amount for a single friend with negative amount to pay")
+    void expectsAmountToBeSettledForNegativeAmount() {
+        Friend friend = new Friend("Pulkit", 100, 0);
+        Friend settledFriend = new Friend("Pulkit", 100, -100);
+
+        friend.settleAmount(-100);
+
+        Assertions.assertEquals(settledFriend, friend);
+    }
+
+    @Test
+    @DisplayName("Should stringify friend properly")
+    void expectsToStringToStringify() {
+        Friend friend = new Friend("Pulkit", 100, 0);
+        String expectedStringifyFriend = "Pulkit gets 100";
+
+        Assertions.assertEquals(expectedStringifyFriend, friend.toString());
+    }
+
+    @Test
+    @DisplayName("Should compare two friends")
+    void expectsEqualsToCompareTwoFriends() {
+        Friend friend = new Friend("Pulkit", 100, 0);
+        Friend anotherFriend = new Friend("Pulkit", 100, 0);
+
+        Assertions.assertEquals(friend, anotherFriend);
+    }
+}
