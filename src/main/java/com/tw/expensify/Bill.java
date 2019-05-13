29c32a56c1ea8aa6f9a26ee59d3bb681fc2161b2
pulkit.gupta <pulkit.gupta@thoughtworks.com>
2019-05-13 18:09:13 +0530

    Add Bill.java

---
+++ src/main/java/com/tw/expensify/Bill.java
@@ -0,0 +1,26 @@
+package com.tw.expensify;
+
+import java.util.List;
+
+class Bill {
+    private final Integer amount;
+    private final BillType type;
+    private final Friend paidBy;
+    private final List<Friend> paidFor;
+
+    Bill(Integer amount, BillType type, Friend paidBy, List<Friend> paidFor) {
+        this.amount = amount;
+        this.type = type;
+        this.paidBy = paidBy;
+        this.paidFor = paidFor;
+    }
+
+    void settleIndividualExpense() {
+        int friendsCount = paidFor.size();
+        int perHeadAmount = amount / friendsCount;
+
+        for (Friend friend : paidFor) {
+            friend.settleAmount(perHeadAmount);
+        }
+    }
+}
