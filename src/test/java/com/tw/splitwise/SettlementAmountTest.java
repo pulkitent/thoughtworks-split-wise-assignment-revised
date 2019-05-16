dfe6a26fb57252afc4245b5c41f93aa6c9d8f92e
pulkit.gupta <pulkit.gupta@thoughtworks.com>
2019-05-16 15:12:49 +0530

    Add test for settlementAmount.java

--- src/test/java/com/tw/splitwise/SettlementAmountTest.java
+++ src/test/java/com/tw/splitwise/SettlementAmountTest.java
@@ -13,4 +13,22 @@ class SettlementAmountTest {
 
         Assertions.assertEquals(expectedStringifiedAmount, settlementAmount.toString());
     }
+
+    @Test
+    @DisplayName("Should find whether two settlement amounts are equal or not")
+    void expectsToCheckEquality() {
+        SettlementAmount settlementAmount = new SettlementAmount(100.0, new Friend("Rahul", 100.0, 100.0));
+        SettlementAmount anotherSettlementAmount = new SettlementAmount(100.0, new Friend("Rahul", 100.0, 100.0));
+
+        Assertions.assertTrue(settlementAmount.equals(anotherSettlementAmount));
+    }
+
+    @Test
+    @DisplayName("Should compute hash of settlement amount properly")
+    void expectedHashToBeComputed() {
+        SettlementAmount settlementAmount = new SettlementAmount(100.0, new Friend("Rahul", 100.0, 100.0));
+        int expectedHash = -814204880;
+
+        Assertions.assertEquals(expectedHash, settlementAmount.hashCode());
+    }
 }
